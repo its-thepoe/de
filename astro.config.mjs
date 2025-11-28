1623ff9d1d98324995f5bce0b3f7fefc07176577
MAZE <remvze@proton.me>
2025-11-28 21:35:54 +0330

    chore: change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: 'https://de.mvze.net',
+  site: 'https://designengineers.net',
 });
